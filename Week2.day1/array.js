7ff2efa44f1df814b8fb52fbfb51e48a6ad3c219
Sarupriya Tharanipathy <103900637+Sarupriya14@users.noreply.github.com>
2026-08-11 23:07:40 +0530

    Add files via upload

---
+++ Week2.day1/array.js
@@ -0,0 +1,12 @@
+const numarray = [1, 2, 3, 4, 5];
+
+
+console.log(numarray.length);
+console.log(numarray.indexOf(4));
+
+console.log(numarray[5] = 'welcome');
+console.log(numarray);
+
+console.log(numarray[2]=true);
+console.log(numarray);
+
---
+++ Week2.day1/stringMethods.js
@@ -0,0 +1,20 @@
+//slice()
+
+let course ='playwright'
+
+let output = course.slice(2,5);
+console.log(output);
+
+
+let output1 = course.slice(4,-2)
+console.log(output1);
+
+
+//substring
+let result2 = course.substring(2,5);
+console.log(result2);
+
+let result3 = course.substring(5,2);
+console.log(result3);
+
+
